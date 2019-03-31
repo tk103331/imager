912745b068d82886002dfbc6b4fe1eb0085e74ed
tk103331 <tk103331@gmail.com>
2019-03-31 22:13:33 +0800

    rotate imager

--- rotate_imager.go
+++ rotate_imager.go
@@ -26,40 +26,36 @@ func (ri *RotateImager) Bounds() image.Rectangle {
 	x3, y3 := ri.rotatePoint(x0, y0, rect.Max.X, rect.Max.Y, ri.radian)
 	x4, y4 := ri.rotatePoint(x0, y0, rect.Min.X, rect.Max.Y, ri.radian)
 
-	dx := max(x1, x2, x3, x4) - min(x1, x2, x3, x4)
-	dy := max(y1, y2, y3, y4) - min(y1, y2, y3, y4)
-	return image.Rect(0, 0, dx, dy)
+	return image.Rect(min(x1, x2, x3, x4), min(y1, y2, y3, y4), max(x1, x2, x3, x4), max(y1, y2, y3, y4))
 }
 
 func (ri *RotateImager) At(x, y int) color.Color {
 	rect := ri.img.Bounds()
 	x0 := rect.Dx() / 2
 	y0 := rect.Dy() / 2
-
 	x1, y1 := ri.rotatePoint(x0, y0, x, y, -ri.radian)
 	return ri.img.At(x1, y1)
 }
 
 func (ri *RotateImager) rotatePoint(x0, y0 int, x, y int, r float64) (int, int) {
 
-	d := distance(x, y, x0, y0)
-	var r0 float64 = 0
-	if x0 == x && y0 == y {
+	var r0 float64
+	if x == x0 && y == y0 {
 		return x, y
-	} else if x0 == x {
+	} else if x == x0 {
 		if y < y0 {
+			r0 = math.Pi / 2
+		} else {
+			r0 = math.Pi * 3 / 2
+		}
+	} else if y == y0 {
+		if x < x0 {
 			r0 = 0
 		} else {
 			r0 = math.Pi
 		}
-	} else if y0 == y {
-		if x < x0 {
-			r0 = math.Pi / 2
-		} else {
-			r0 = 0
-		}
 	} else {
-		r0 := math.Abs(math.Atan(float64(y-y0) / float64(x-x0)))
+		r0 = math.Abs(math.Atan(float64(y-y0) / float64(x-x0)))
 		if x < x0 && y < y0 {
 			r0 = r0
 		} else if x > x0 && y < y0 {
@@ -77,16 +73,9 @@ func (ri *RotateImager) rotatePoint(x0, y0 int, x, y int, r float64) (int, int) 
 		r1 = r1 - math.Pi*2
 	}
 
+	d := distance(x, y, x0, y0)
 	x1, y1 := 0, 0
-	if r1 == 0 {
-		return int(float64(x0) - d), y0
-	} else if r1 == math.Pi/2 {
-		return x0, int(float64(y0) - d)
-	} else if r1 == math.Pi {
-		return int(float64(x0) + d), y0
-	} else if r1 == math.Pi*3/2 {
-		return x0, int(float64(y0) + d)
-	} else if 0 < r1 && r1 < math.Pi/2 {
+	if 0 < r1 && r1 < math.Pi/2 {
 		r1 = r1
 		x1 = x0 - int(math.Cos(r1)*d)
 		y1 = y0 - int(math.Sin(r1)*d)
